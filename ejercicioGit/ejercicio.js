63d446caafcd7c64196a65e2479ae46b00e5f4ae
warkosec <warkosgaming.ec@gmail.com>
2026-01-13 23:59:39 -0500

    Se agrego una imagen al usar boton

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,3 +1,6 @@
 saludar=function(){
+    let cmpImagenSaludi;
+    cmpImagenSaludo=document.getElementById("imagenSaludo");
+    cmpImagenSaludo.src="./imagenes/dancing-banana.gif";
     console.log("Hola Mundo");
 }
