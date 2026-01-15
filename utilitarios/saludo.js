e16ba9b2e39bdcc93431cb1290ca6aade1a91dd3
warkosec <warkosgaming.ec@gmail.com>
2026-01-14 19:56:39 -0500

    Se implemento recuperar int y float

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -3,6 +3,10 @@ saludar=function(){
     let nombre = recuperarTexto('txtNombre');
     //recuperar el valor de la caja de texto txtApellido
     let apellido = recuperarTexto('txtApellido');
+    let edad = recuperarInt('txtEdad');
+    let estatura = recuperarFloat('txtEstatura');
+
+
 }
 recuperarTexto = function(idComponente){
     let componente;
@@ -10,4 +14,14 @@ recuperarTexto = function(idComponente){
     componente = document.getElementById(idComponente);
     valorIngresador = componente.value;
     return valorIngresador;
+}
+recuperarInt = function(idComponente){
+        let valorCaja=recuperarTexto(idComponente);
+        let valorEntero=parseInt(valorCaja);
+        return valorEntero;
+}
+recuperarFloat = function(idComponente){
+    let valorCaja=recuperarTexto(idComponente);
+    let valorFloat=parseFloat(valorCaja);
+    return valorFloat;
 }
