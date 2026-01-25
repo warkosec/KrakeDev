a3d9ee60930ee97a94b98a38cce3fb1e88eb959b
warkosec <warkosgaming.ec@gmail.com>
2026-01-25 12:03:07 -0500

    actualizacion

--- puntoDeVentaV2/serviciosPuntoDeVenta.js
+++ puntoDeVentaV2/serviciosPuntoDeVenta.js
@@ -13,3 +13,23 @@ calcularSubtotal = function (precio, cantidad) {
 calcularTotal = function (subtotal, descuento, iva) {
     return subtotal - descuento + iva;
 }
+
+/*
+    NUEVA FUNCIÓN (Reto: descuento por volumen)
+    Retorna el MONTO de descuento, no porcentaje.
+*/
+calcularDescuentoPorVolumen = function (subtotal, cantidad) {
+    let porcentaje = 0;
+
+    if (cantidad < 3) {
+        porcentaje = 0;
+    } else if (cantidad >= 3 && cantidad <= 5) {
+        porcentaje = 0.03;
+    } else if (cantidad >= 6 && cantidad <= 11) {
+        porcentaje = 0.04;
+    } else {
+        porcentaje = 0.05;
+    }
+
+    return subtotal * porcentaje;
+}
